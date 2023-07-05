0ca59ab24cbf1205bbb3f43c93a9a1b34a54d57b
iansw246 <35947093+iansw246@users.noreply.github.com>
2023-07-05 14:41:51 -0700

    Create test.js with error schema

---
+++ test.js
@@ -0,0 +1,14 @@
+const { compile } = require('.');
+
+const schema = {
+  "title": "2022 Example Schema",
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "string",
+      "example": "61e36b8fc12c4d8fd0842f76"
+    }
+  }
+};
+
+compile(schema).then(ts => console.log(ts));
