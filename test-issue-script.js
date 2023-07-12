b21c60095ac373cad4f3e6134f7cb104cd0960a8
iansw246 <35947093+iansw246@users.noreply.github.com>
2023-07-12 18:53:19 +0000

    Add schema for #185 and #151

--- test-issue-script.js
+++ test-issue-script.js
@@ -1,14 +1,55 @@
 const { compile } = require('./dist/src');
 
-const schema = {
-  "title": "2022 Example Schema",
+// const schema = {
+//   "title": "2022 Example Schema",
+//   "type": "object",
+//   "properties": {
+//     "id": {
+//       "type": "string",
+//       "example": "61e36b8fc12c4d8fd0842f76"
+//     }
+//   }
+// };
+
+// compile(schema).then(ts => console.log(ts));
+
+const schema2 = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
   "type": "object",
+  "title": "Product Export Schema",
   "properties": {
-    "id": {
-      "type": "string",
-      "example": "61e36b8fc12c4d8fd0842f76"
+    "products": {
+      "type": "array",
+      "title": "Products",
+      "items": {
+        "oneOf": [
+          {
+            "type": "object",
+            "title": "3D Model",
+            "properties": {
+              "type": {
+                "type": "string",
+                "title": "Type",
+                "constant": "3d_surface_model"
+              },
+              "description": {
+                "type": "string",
+                "title": "Description",
+                "default": ""
+              },
+              "status": {
+                "type": "string",
+                "title": "200",
+                "example": "200",
+                "description": "...."
+              }
+            }
+          }
+        ]
+      },
+      "minItems": 1
     }
   }
-};
+}
 
-compile(schema).then(ts => console.log(ts));
+compile(schema2).then(ts => console.log(ts));
